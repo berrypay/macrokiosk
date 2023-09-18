668a7fee32210fe5259bab293198a546e3fed479
San <mr.otkilbaev@gmail.com>
2023-09-18 10:57:32 +0000

    Added changes to subtests and refactored the code

--- send_test.go
+++ send_test.go
@@ -16,15 +16,42 @@ package macrokiosk
 import "testing"
 
 func TestDecodeSingleMTResponse(t *testing.T) {
-	var (
-		sampleString = "60132899698,26556334019,200,MYR,0.0900|=9.4450,1"
-	)
 
-	result, err := decodeSingleMTResponse([]byte(sampleString))
+	// ----------- Old Test ---------------
+	// var (
+	// 	sampleString = "60132899698,26556334019,200,MYR,0.0900|=9.4450,1"
+	// )
 
-	if result == nil && err != nil {
-		t.Errorf("Expected successful return, got result: %v and error: %v", result, err.Error())
-	}
+	// result, err := decodeSingleMTResponse([]byte(sampleString))
+
+	// if result == nil && err != nil {
+	// 	t.Errorf("Expected successful return, got result: %v and error: %v", result, err.Error())
+	// }
+
+	t.Run("Positive case", func(t *testing.T) {
+		const arraySize int = 3
+		correctSampleStrings := [arraySize]string{"60132899698,26556334019,200,MYR,0.0900|=9.4450,1", " 60132899698 , 26556334019 , 200, MYR , 0.0900 |= 9.4450,1 ", "60132899698, 26556334019,200,myr,0.0900|=9.4450,1"}
+
+		for i, _ := range correctSampleStrings {
+			result, err := decodeSingleMTResponse([]byte(correctSampleStrings[i]))
+
+			if result == nil && err != nil {
+				t.Errorf("Expected successful return, got result: %v and error: %v", result, err.Error())
+			}
+		}
+	})
+	t.Run("Error case", func(t *testing.T) {
+		const arraySize int = 5
+		notCorrectSampleStrings := [arraySize]string{"abc456,26556334019,200,MYR,0.0900|=9.4450,1", "60132899698, abc456, 200,MYR,0.0900|=9.4450,1", "60132899698, 26556334019, abc456,MYR,0.0900|=9.4450,1", "60132899698, 26556334019, 200,RANDOM,0.0900|=9.4450,1", "60132899698, 26556334019, 200,MYR,abc456|=9.4450,1"}
+
+		for i, _ := range notCorrectSampleStrings {
+			result, err := decodeSingleMTResponse([]byte(notCorrectSampleStrings[i]))
+
+			if result != nil && err == nil {
+				t.Errorf("Expected unsuccesfull return, instead of it it was processed, got result: %v", result)
+			}
+		}
+	})
 }
 
 func TestDecodeMultiMTResponse(t *testing.T) {
